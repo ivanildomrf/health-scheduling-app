bff52823ac94bbb645b7441fbb4c456d6374590d
Ivanildo Mendonça <ivanildomrf@gmail.com>
2025-05-27 23:44:48 -0300

    feat: implementar autenticação e redirecionamento nas páginas principais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <Button>Hello World</Button>;
-}
+const Home = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session || !session?.user) {
+    redirect("/authentication");
+  }
+};
+
+export default Home;
