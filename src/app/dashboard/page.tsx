499ffc27b21b56688cd3cfc0abaf8757da282d3f
Ivanildo Mendonça <ivanildomrf@gmail.com>
2025-05-28 01:16:55 -0300

    feat: adicionar login com Google e exibir imagem do usuário no dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,9 @@ import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import { SignOutButton } from "./components/sign-out-button";
-
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -28,6 +28,13 @@ const DashboardPage = async () => {
       <h1>Dashboard</h1>
       <h1>{session?.user.name}</h1>
       <h1>{session?.user.email}</h1>
+      <Image
+        src={(session?.user.image as string) || ""}
+        alt="User"
+        width={100}
+        height={100}
+        className="rounded-full"
+      />
       <SignOutButton />
     </div>
   );
