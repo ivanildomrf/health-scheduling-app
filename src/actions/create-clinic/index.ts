19a2d1b894494c237ea8f5d3b902a802e149f0c8
Ivanildo Mendonça <ivanildomrf@gmail.com>
2025-05-28 00:41:05 -0300

    feat: adicionar funcionalidade de criação de clínicas e formulário correspondente

---
+++ src/actions/create-clinic/index.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { db } from "@/db";
+import { clinicsTable, usersToClinicsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export const createClinic = async (name: string) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session || !session.user) {
+    return new Error("Unauthorized");
+  }
+
+  const [clinic] = await db
+    .insert(clinicsTable)
+    .values({
+      id: crypto.randomUUID(),
+      name,
+    })
+    .returning();
+
+  await db.insert(usersToClinicsTable).values({
+    userId: session.user.id,
+    clinicId: clinic.id,
+  });
+
+  redirect("/dashboard");
+};
---
+++ src/app/clinic-form/components/form.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { createClinic } from "@/actions/create-clinic";
+import { Button } from "@/components/ui/button";
+import { DialogFooter } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const clinicFormSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+});
+
+const ClinicForm = () => {
+  const form = useForm<z.infer<typeof clinicFormSchema>>({
+    resolver: zodResolver(clinicFormSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof clinicFormSchema>) => {
+    const result = await createClinic(data.name);
+
+    if (result instanceof Error) {
+      console.error(result.message);
+      toast.error("Erro ao criar clínica");
+    }
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <DialogFooter className="space-y-2">
+            <Button type="submit" disabled={form.formState.isSubmitting}>
+              {form.formState.isSubmitting && !form.formState.isDirty && (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              )}{" "}
+              Criar clínica
+            </Button>
+          </DialogFooter>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default ClinicForm;
